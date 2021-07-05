7f0e0edc8f7928d1ec6213415624a28c2a9a7f9b
GuzMichal <mic.raf.guz@gmail.com>
2021-07-05 16:53:25 +0200

    po sprawdzeniu

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Name from "./Name";
 import Button from "./Button";
-import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
